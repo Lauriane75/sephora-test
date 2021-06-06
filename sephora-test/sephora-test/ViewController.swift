7d2e28750bd4d3c2f051c6fcb1cd6d93550d5fcb
Lauriane <lauriane.haydari@gmail.com>
2021-06-06 20:01:12 +0200

    Add initial project

---
+++ sephora-test/sephora-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sephora-test
+//
+//  Created by Lau on 06/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
